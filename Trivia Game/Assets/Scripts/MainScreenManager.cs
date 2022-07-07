9a57e9cd8e4ee27558b47d0bc313279915ab251b
OmarAbushaban <omarghalib@gmail.com>
2022-07-07 17:20:37 +0300

    Added the main screen UI

---
+++ Trivia Game/Assets/Scripts/MainScreenManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MainScreenManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
